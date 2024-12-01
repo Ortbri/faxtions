f60ce5a057b1067b90a39efc4e89190c5fdd75f7
ortbri <brian.ort02@icloud.com>
2024-12-01 12:58:35 -0600

    long shot, but executorch

--- metro.config.js
+++ metro.config.js
@@ -5,5 +5,11 @@ const config = getDefaultConfig(__dirname);
 
 // Add any custom configurations here
 config.resolver.assetExts.push('db');
+/**
+ * If you plan on adding your models to the assets instead of
+ * fetching them from a url, you also need to add following lines to your metro.config.js:
+ */
+defaultConfig.resolver.assetExts.push('pte');
+defaultConfig.resolver.assetExts.push('bin');
 
 module.exports = config;
